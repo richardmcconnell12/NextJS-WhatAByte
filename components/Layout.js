49918c2618787af36c31a63f9a62e926d449e152
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-10-08 15:57:25 -0500

    part 2 complete -- looks good

--- components/Layout.js
+++ components/Layout.js
@@ -1,32 +1,27 @@
-import Header from "./Header";
-import NavBar from "./NavBar";
 import Head from "next/head";
 
-const layoutStyle = {
-    display: "flex",
-    flexDirection: "column",
-    height: "100%",
-    width: "100%"
-};
+import Header from "./Header";
+import NavBar from "./NavBar";
 
-const contentStyle = {
-    flex: 1,
-    display: "flex",
-    flexDirection: "column"
-};
+import "./Layout.scss";
+import "./index.scss";
 
-const Layout = props => (
-    <div className="Layout" style={layoutStyle}>
-        <Head>
-            <title>WHATABYTE</title>
-        </Head>
+const Layout = props => {
+    const appTitle = `> WHATABYTE`;
 
-        <Header />
-        <div className="Content" style={contentStyle}>
-            {props.children}
+    return (
+        <div className="Layout">
+            <Head>
+                <title>WHATABYTE</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
+            </Head>
+
+            <Header appTitle={appTitle} />
+            <div className="Content">{props.children}</div>
+            <NavBar />
         </div>
-        <NavBar />
-    </div>
-);
+    );
+};
 
 export default Layout;
