dccc91a24a509bbd7f7788ae3f55541636ec23d2
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-11-05 12:16:01 -0600

    cleaned up unused code

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,6 @@
 import "./NavBar.scss";
 import NavButton from "./NavButton";
 
-const navBarStyle = {
-  backgroundColor: "red",
-  color: "white",
-  width: "100%",
-  height: "60px"
-};
-
 const NavBar = props => (
   <div className="NavBar">
     {props.navButtons.map(button => (
